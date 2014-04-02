5cbc0c59c38db049533077324226ba9509e34754
JeremyLetang <letang.jeremy@gmail.com>
2014-04-02 11:58:14 -0400

    restore source deleted by a stupid commit...

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+//! Graph library for Rust
+//!
+//! Provide an abstract Graph library and graph file output
+//!
+//!
+//!
+
+#![crate_id = "rgraph#0.1"]
+#![desc = "Graph library in Rust"]
+#![license = "MIT"]
+#![crate_type = "lib"]
+
+#![deny(non_camel_case_types)]
+#![deny(non_uppercase_statics)]
+#![deny(unnecessary_qualification)]
+// #[warn(missing_doc)];
+
+extern crate serialize;
+
+// public reexports
+pub use graph::Graph;
+pub use graph::{Vertex, VertexIterator};
+pub use graph::{Edge, EdgeIterator};
+
+// mods
+mod graph;
+// pub mod graphviz;
+// pub mod graphml;
